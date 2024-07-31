c17491b4bd93ad74be17bcfc88293ef99d9aa6d7
Robin Burri <robinburri@tutanota.com>
2024-07-31 11:23:54 +0200

    Add main Activity

---
+++ app/src/main/java/com/example/ft_hangouts/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.ft_hangouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
